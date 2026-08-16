2326313a872a8c455dbfdb6719670dd3dda36ebf
napthehungakira <trungbuir2008@gmail.com>
2026-08-17 00:15:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Đã cập nhật đúng thông tin Partner ID và Partner Key từ Gachthe1s của bạn
+// Thông tin chuẩn từ Gachthe1s của bạn
 const PARTNER_ID = "2836866755";
-const PARTNER_KEY = "e3c434a49e4b1279f0dc40a46b7e07a9e";
+const PARTNER_KEY = "e3c434a49e4b1279f0dc40a6b7e07a9e";
 
 app.post('/api/nap-the', async (req, res) => {
     try {
